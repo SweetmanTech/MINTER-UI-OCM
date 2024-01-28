8b451333078c0da5359700409b8a51440c59f897
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-28 16:25:51 -0300

    I add text on first time UX.

--- components/Pages/LandingPage/LandingPage.tsx
+++ components/Pages/LandingPage/LandingPage.tsx
@@ -10,21 +10,21 @@ export default function Component() {
     <section className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center gap-11 text-center">
-          <ConnectButton label="Sign Up" />
-          {signer && (
-            <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                Zora 💸 USDC
-              </h1>
-              <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
-                Start accepting USDC payments for your imagination on{" "}
-                <a href="https://zora.co/" target="_blank" rel="noreferrer">
-                  Zora.
-                </a>
-              </p>
-            </div>
-          )}
-          {signer && (
+          {signer && <ConnectButton label="Sign Up" />}
+
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+              Zora 💸 USDC
+            </h1>
+            <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+              Start accepting USDC payments for your imagination on{" "}
+              <a href="https://zora.co/" target="_blank" rel="noreferrer">
+                Zora.
+              </a>
+            </p>
+          </div>
+
+          {signer ? (
             <div className="w-full max-w-sm space-y-2">
               <form className="flex space-x-2">
                 <Input
@@ -32,9 +32,11 @@ export default function Component() {
                   placeholder="Enter collection address"
                   type="text"
                 />
-                <Button type="submit">Upgrade</Button>
+                <Button type="submit">Search</Button>
               </form>
             </div>
+          ) : (
+            <ConnectButton label="Sign Up" />
           )}
         </div>
       </div>
