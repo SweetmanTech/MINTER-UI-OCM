cbba0abee2c54a3c9eaa94d1546170f03f73326b
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-28 16:21:04 -0300

    I init Landing Page UI.

--- components/Pages/LandingPage/LandingPage.tsx
+++ components/Pages/LandingPage/LandingPage.tsx
@@ -1,25 +1,41 @@
 import Button from "@/components/Button"
 import Input from "@/components/Input"
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+import { useEthersSigner } from "onchain-magic"
 
 export default function Component() {
+  const signer = useEthersSigner()
+
   return (
     <section className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
-        <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <div className="space-y-2">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-              Collection Lookup
-            </h1>
-            <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
-              Enter your collection address to start accepting USDC payments.
-            </p>
-          </div>
-          <div className="w-full max-w-sm space-y-2">
-            <form className="flex space-x-2">
-              <Input className="max-w-lg flex-1" placeholder="Enter item name" type="text" />
-              <Button type="submit">Search</Button>
-            </form>
-          </div>
+        <div className="flex flex-col items-center justify-center gap-11 text-center">
+          <ConnectButton label="Sign Up" />
+          {signer && (
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                Zora 💸 USDC
+              </h1>
+              <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+                Start accepting USDC payments for your imagination on{" "}
+                <a href="https://zora.co/" target="_blank" rel="noreferrer">
+                  Zora.
+                </a>
+              </p>
+            </div>
+          )}
+          {signer && (
+            <div className="w-full max-w-sm space-y-2">
+              <form className="flex space-x-2">
+                <Input
+                  className="max-w-lg flex-1"
+                  placeholder="Enter collection address"
+                  type="text"
+                />
+                <Button type="submit">Upgrade</Button>
+              </form>
+            </div>
+          )}
         </div>
       </div>
     </section>
